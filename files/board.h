5720d21744b8ec59bb1d7c4bfa438fda4aecc098
Matthew Leandres <matthewleandres@v1020-wn-174-16.campus-dynamic.uwaterloo.ca>
2017-07-11 16:53:08 -0400

    worked on entity.h

--- files/board.h
+++ files/board.h
@@ -1,14 +1,23 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
 #include <string>
 #include <iostream>
 #include <map>
+#include "entity.h"
 
 struct Posn {
 	unsigned int x, y;
 
 	Posn operator<(const Posn &rhs) const;
-}
+};
 
 
 class Board {
+	Map<Posn, Entity*> ents;
 
-}
+
+
+};
+
+#endif
--- files/entity.h
+++ files/entity.h
@@ -1,7 +1,13 @@
+#ifndef __ENTITY_H__
+#define __ENTITY_H__
+
 #include "board.h"
 
+// Entity is an abstract class
 class Entity {
 	int x, y;
+	char icon;
 
 }
 
+#endif
